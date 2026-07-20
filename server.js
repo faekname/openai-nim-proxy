1430aef2f49e6be86b97ee2ef3079e16a1e2a45d
faekname <faekname08@gmail.com>
2026-07-20 07:40:59 -0700

    Update server.js

--- server.js
+++ server.js
@@ -22,13 +22,12 @@ const ENABLE_THINKING_MODE = false; // Set to true to enable chat_template_kwarg
 
 // Model mapping (adjust based on available NIM models)
 // stepfun-ai/step-3.7-flash
-// deepseek-ai/deepseek-v4-flash
 // deepseek-ai/deepseek-v4-pro
 const MODEL_MAPPING = {
   'gpt-3.5-turbo': 'nvidia/llama-3.1-nemotron-ultra-253b-v1',
   'gpt-4': 'qwen/qwen3-coder-480b-a35b-instruct',
   'gpt-4-turbo': 'z-ai/glm-5',
-  'gpt-4o': 'deepseek-ai/deepseek-v4-pro',
+  'gpt-4o': 'z-ai/glm-5.2',
   'claude-3-opus': 'openai/gpt-oss-120b',
   'claude-3-sonnet': 'openai/gpt-oss-20b',
   'gemini-pro': 'qwen/qwen3-next-80b-a3b-thinking',
